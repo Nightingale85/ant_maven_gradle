f29930d1de07c2eb81682e29b8363e9f67f6426f
Sergey Solovyov <solovyov@sergey.com>
2016-08-16 12:59:15 +0300

    add project

---
+++ src/test/java/com/TestFalse.java
@@ -0,0 +1,18 @@
+package com;
+
+import org.junit.Test;
+
+ import java.util.Random;
+
+ import static org.junit.Assert.assertEquals;
+
+
+     public class TestFalse {
+
+     private Random random = new Random();
+
+             @Test
+      public void testIsEmptyTrue(){
+               assertEquals(true, true);
+       }
+     }
